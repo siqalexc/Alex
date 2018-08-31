90e712c6334bd6983934c4335c7d6c541a22dacd
Alex Cheng <alex@sleepiqlabs.com>
2018-08-31 14:50:51 -0700

    Initial commit

---
+++ source/c/test.c
@@ -0,0 +1,20 @@
+/*
+ * test optarg call
+ *
+ * 08/30/2018 by Alex Cheng
+ */
+ 
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("cmd input 1 - argc = %d, argv = %s \n", argc, argv[0]);
+    getopt(argc, argv, "-");
+    printf("command input - %s\n", optarg);
+    return 0;
+}
+
+
